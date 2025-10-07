8d87cfdd454605e9a653bd17b709eab002685118
sofiasannino <sofia.sannino@gmail.com>
2025-10-07 15:06:16 +0200

    ex2,3

--- labs/ex02/template/stochastic_gradient_descent.py
+++ labs/ex02/template/stochastic_gradient_descent.py
@@ -19,11 +19,18 @@ def compute_stoch_gradient(y, tx, w):
         An array of shape (2, ) (same shape as w), containing the stochastic gradient of the loss at w.
     """
 
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: implement stochastic gradient computation. It's the same as the usual gradient.
-    # ***************************************************
-    raise NotImplementedError
+    # implement stochastic gradient computation. It's the same as the usual gradient.
+
+    # batch size
+    B=len(y)
+
+    #compute error
+    e=y - tx @ w
+
+    #compute gradient
+    grad=(-1/B)* (tx.T @ e)
+
+    return grad
 
 
 def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
@@ -48,13 +55,20 @@ def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
     w = initial_w
 
     for n_iter in range(max_iters):
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: implement stochastic gradient descent.
-        # ***************************************************
-        raise NotImplementedError
+        #compute current loss
+       loss= compute_loss(y, tx, w)
+       
+       # compute gradient and update w
+       for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
+                grad= compute_stoch_gradient(minibatch_y, minibatch_tx, w)
+                w = w - gamma*grad
+                
+       # store w and loss
+       ws.append(w)
+       losses.append(loss)
+        
 
-        print(
+    print(
             "SGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
                 bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
             )
--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -6,10 +6,27 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+    """
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        degree: integer.
+
+    Returns:
+        poly: numpy array of shape (N,d+1)
+    
+    >>> build_poly(np.array([0.0, 1.5]), 2)
+    array([[1.  , 0.  , 0.  ],
+           [1.  , 1.5 , 2.25]])
+    """
+    #dimensions
+    N = len(x)
+    d = degree
+
+    #creating the matrix
+    Phi = np.zeros((N, d + 1))
+
+    for j in range(d + 1) : 
+        Phi[:, j] = x[:, ] ** j
+
+    return Phi
+    
